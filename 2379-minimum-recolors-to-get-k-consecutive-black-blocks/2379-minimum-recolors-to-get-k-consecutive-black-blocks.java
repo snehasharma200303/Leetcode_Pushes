783082a663748a137e8a7141c5898f99723cdc41
Sneha Sharma <145634394+snehasharma200303@users.noreply.github.com>
2025-03-08 08:54:37 +0530

    Time: 3 ms (17.03%), Space: 41.6 MB (58.45%) - LeetHub

--- 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -4,6 +4,7 @@ class Solution {
        for(int i=0;i<s.length()-k+1;i++){
         int currMin=0;
         for(int j=0;j<k;j++){
+            if(minWays==0) return minWays;
             if(s.charAt(i+j)=='W') currMin++;
             
         }
