0fa1ca630477aedd767486df76b43ec5eb015ffc
Sneha Sharma <145634394+snehasharma200303@users.noreply.github.com>
2025-03-16 21:09:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
@@ -1,14 +1,35 @@
 class Solution {
-    public int maximumCandies(int[] a, long k) {
-         int lo = 1, hi = 0, ans = 0;
-        for (int x : a) hi = Math.max(hi, x);
-        while (lo <= hi) {
-            int mid = (lo + hi) >>> 1;
-            long cnt = 0;
-            for (int x : a) cnt += x / mid;
-            if (cnt >= k) { ans = mid; lo = mid + 1; }
-            else hi = mid - 1;
+    public int maximumCandies(int[] candies, long k) {
+        int left=1, right=10000000;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(isValidCandy(candies,k,mid)){
+                left=mid+1;
+            }
+            else right=mid-1;
         }
-        return ans;
+        return right;
+    }
+    private boolean isValidCandy(int[] arr, long k, int mid){
+        int child=0;
+        for(int val:arr){
+           child+=val/mid;
+            if(child>=k) return true;
+        }
+        return false;
     }
 }
+// class Solution {
+//     public int maximumCandies(int[] a, long k) {
+//          int lo = 1, hi = 0, ans = 0;
+//         for (int x : a) hi = Math.max(hi, x);
+//         while (lo <= hi) {
+//             int mid = (lo + hi) >>> 1;
+//             long cnt = 0;
+//             for (int x : a) cnt += x / mid;
+//             if (cnt >= k) { ans = mid; lo = mid + 1; }
+//             else hi = mid - 1;
+//         }
+//         return ans;
+//     }
+// }
