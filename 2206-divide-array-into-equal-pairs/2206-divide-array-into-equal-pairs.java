959fc0c2332e5e847902babf8a3726eb3a7f923d
Sneha Sharma <145634394+snehasharma200303@users.noreply.github.com>
2025-03-17 14:14:49 +0530

    Time: 5 ms (48.55%), Space: 44.2 MB (85.16%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
@@ -1,13 +1,28 @@
-class Solution {
+// class Solution {
+//     public boolean divideArray(int[] nums) {
+//         if(nums.length%2 != 0) return false;
+//         HashMap<Integer,Integer> map=new HashMap<>();
+//         for(int val: nums){
+//             map.put(val,map.getOrDefault(val,0)+1);
+//         }
+//         for(int v: map.keySet()){
+//             if(map.get(v) %2 != 0) return false;
+//         }
+//         return true;
+//     }
+// }
+
+ class Solution {
     public boolean divideArray(int[] nums) {
-        if(nums.length%2 != 0) return false;
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int val: nums){
-            map.put(val,map.getOrDefault(val,0)+1);
-        }
-        for(int v: map.keySet()){
-            if(map.get(v) %2 != 0) return false;
+        Arrays.sort(nums);
+        int count=1;
+        for(int i=1;i<nums.length;i++){
+           if(nums[i]==nums[i-1]) count++;
+           else if(nums[i] != nums[i-1]) {
+            if(count%2 !=0 ) return false;
+            else count=1;
+           }
         }
         return true;
     }
-}
+    }
