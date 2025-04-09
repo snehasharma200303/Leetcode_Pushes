063a563a2df964d6186229704268804f5e7d8a7a
Sneha Sharma <145634394+snehasharma200303@users.noreply.github.com>
2025-04-09 10:41:33 +0530

    Time: 17 ms (56.86%), Space: 44.9 MB (95.51%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,25 +1,25 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int l=1,r=0;
+        long l=1,r=0;
         int k=0;
         for(int p: piles) r+=p;
         while(l<=r){
-            int mid=l+(r-l)/2;
+            long mid=l+(r-l)/2;
             if(isValidK(piles,mid,h)){
-                k=mid;
+                k=(int)mid;
                 r=mid-1;
             }
             else l=mid+1;
         }
         return k;
     }
-    public boolean isValidK(int[] piles,int mid, int h){
+    public boolean isValidK(int[] piles,long mid, int h){
         int hr=0;
             for(int i=0;i<piles.length;i++){
                 if(mid>=piles[i]) hr++;
                 else{
-                    hr+=piles[i]/mid;
-                    if(piles[i]%mid <mid) hr++;
+                   hr+=piles[i]/mid;
+                   if(piles[i]%mid != 0) hr++;      
                 }
             }
             if(hr<=h) return true;
