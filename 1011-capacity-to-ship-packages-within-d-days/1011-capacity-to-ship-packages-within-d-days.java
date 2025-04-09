e23474adf623405216b1fbe7198da284bd2a077d
Sneha Sharma <145634394+snehasharma200303@users.noreply.github.com>
2025-04-09 15:41:54 +0530

    Time: 5 ms (99.83%), Space: 48.1 MB (5.94%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,37 +1,66 @@
 class Solution {
-    public int shipWithinDays(int[] weights, int days) {
-        long l=Integer.MIN_VALUE,r=0;
-        int ans=0;
-        for(int w:weights){
-          r+=(long)w;  
-          l=Math.max(l,w);
-        } 
-        //System.out.println(l);
-        while(l<=r){
-            long mid=l+(r-l)/2;
-            if(isValidW(weights,mid,days)){
-                ans=(int)mid;
-                r=mid-1;
-            }
-            else l=mid+1;
+    // public int shipWithinDays(int[] weights, int days) {
+    //     long l=Integer.MIN_VALUE,r=0;
+    //     int ans=0;
+    //     for(int w:weights){
+    //       r+=(long)w;  
+    //       l=Math.max(l,w);
+    //     } 
+    //     //System.out.println(l);
+    //     while(l<=r){
+    //         long mid=l+(r-l)/2;
+    //         if(isValidW(weights,mid,days)){
+    //             ans=(int)mid;
+    //             r=mid-1;
+    //         }
+    //         else l=mid+1;
+    //     }
+    //     return ans;
+    // }
+    // public boolean isValidW(int[] weights,long mid, int days){
+    //     int d=1;
+    //     int cap=0;
+    //     for(int i=0;i<weights.length;i++){
+    //         if(cap+weights[i] <= mid){
+    //             cap+=weights[i];
+    //         }
+    //         else{
+    //             d++;
+    //             cap=weights[i];
+    //         }
+    //     }
+    //     if(d<=days) return true;
+    //     return false;
+    // }
+     public static int shipWithinDays(int[] weights, int days) {
+        int total = 0;
+        int heaviest = 0;
+        for (int weight : weights) {
+            total += weight;
+            heaviest = Math.max(heaviest,weight);
         }
-        return ans;
+        int left = Math.max(heaviest, total/days);
+        int right = heaviest * (int) Math.ceil((double)weights.length/days);
+        while (left < right) {
+            int mid = left + (right-left) / 2;
+            int daysNeeded = totalDay(weights,mid);
+            if (daysNeeded <= days) {
+                right = mid;
+            } else {
+                left = mid+1;
+            }
+        }
+        return left;
     }
-    public boolean isValidW(int[] weights,long mid, int days){
-        int d=1;
-        int cap=0;
-        for(int i=0;i<weights.length;i++){
-            // if(cap >= mid) return; 
-            // cap+=weights[i];
-            if(cap+weights[i] <= mid){
-                cap+=weights[i];
+    private static int totalDay(int[] weights, int capacity) {
+        int days = 1, loaded = capacity;
+        for (int i = 0, n = weights.length; i < n; i++) {
+            if (loaded < weights[i]) {
+                days++;
+                loaded = capacity;
             }
-            else{
-                d++;
-                cap=weights[i];
-            }
+            loaded -= weights[i];
         }
-        if(d<=days) return true;
-        return false;
+        return days;
     }
 }
