b5a66a163116aa1837add847ecffbbdc22469cc9
Sneha Sharma <145634394+snehasharma200303@users.noreply.github.com>
2025-04-25 08:05:33 +0530

    Time: 23 ms (44.62%), Space: 45.3 MB (35.53%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -4,25 +4,20 @@ class Solution {
         int l=0,r=0;
       
         for(int i=0;i<s.length();i++){
-              boolean flag=true;
-             l=i;r=i;
-            while(l>= 0 && r<s.length()){
-                char ch1=s.charAt(l);
-                char ch2=s.charAt(r);
-                if(ch1 != ch2){
-                    flag=false;
-                   break; 
-                } 
-                else{
-                    l--;
-                    r++;
-                }
-            }
-            if(flag){
-                if(ans.length()<(r-l+1)){
-                    ans=s.substring(l+1,r);
+                l=i;r=i;
+            while(l>= 0 && r<s.length() && s.charAt(l) == s.charAt(r)){
+                if(ans.length()<r-l+1){
+                    ans=s.substring(l,r+1);
                     }
+                    l--;r++;
             }
+           l=i;r=i+1;
+                while(l>= 0 && r<s.length() && s.charAt(l) == s.charAt(r)){
+                if(ans.length()<r-l+1){
+                    ans=s.substring(l,r+1);
+                    }
+                    l--;r++;
+            } 
         }
         return ans;
     }
