d7abcc65532aaf895e5fc7ca98ac0ee004e01ea7
Sneha Sharma <145634394+snehasharma200303@users.noreply.github.com>
2025-05-12 12:26:58 +0530

    Time: 271 ms (32.17%), Space: 45.6 MB (15.91%) - LeetHub

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.java
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.java
@@ -5,6 +5,7 @@ class Solution {
         for(int i=0;i<n;i++){
              if(digits[i] == 0) continue;
             for(int j=0;j<n;j++){
+                 if(digits[i] == 0) continue;
               if(i==j) continue;  
                 for(int k=0;k<n;k++){
                     if(i==k || j==k) continue;  
