5c15654a11e409451eb6c100c266055b29f40a66
Sneha Sharma <145634394+snehasharma200303@users.noreply.github.com>
2025-05-21 16:41:00 +0530

    Time: 106 ms (1.59%), Space: 41.2 MB (40.04%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,18 +1,21 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length<=1) return nums[0];
-        int[] dp1=new int[nums.length];
-        int[] dp2=new int[nums.length];
-        int rob1= robber(nums,0,nums.length-1,dp1);
-        int rob2= robber(nums,1,nums.length,dp2);
-        return Math.max(rob1,rob2);
+        int n=nums.length;
+        if(n==1) return nums[0];
+        int[] dp1=new int[n];
+        int[] dp2=new int[n];
+        for(int i=0;i<n;i++) {
+            dp1[i]=-1;
+            dp2[i]=-1;
+        }
+        int first=findRobber(dp1,nums,0,n-1);
+        int second=findRobber(dp2,nums,1,n);
+       return  Math.max(first,second);
     }
-    public int robber(int[] nums, int i,int j,int[] dp){
-        if(i>= j) return 0;
-         if(dp[i] != 0) return dp[i];
-        int f=nums[i]+robber(nums,i+2,j,dp);
-       int l=robber(nums,i+1,j,dp);
-        dp[i]= Math.max(f,l);
+    public int findRobber(int[] dp,int[] nums, int i, int n){
+        if(i>= n) return 0;
+        if(dp[i] != -1) return dp[i];
+        dp[i] =Math.max(nums[i]+findRobber(dp,nums,i+2,n),findRobber(dp,nums,i+1,n));
         return dp[i];
     }
 }
