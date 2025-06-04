22556834d3fbe92ce4d727161e71863997126f95
Sneha Sharma <145634394+snehasharma200303@users.noreply.github.com>
2025-06-05 00:27:39 +0530

    Time: 0 ms (100%), Space: 40.1 MB (91.5%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp=new int[n+1];
-        for(int i=0;i<n+1;i++){
-            dp[i]=-1;
-        }
-        return findPath(n,dp); 
+       int prev1=1;
+       int prev2=1;
+      for(int i=2;i<=n;i++){
+        int temp=prev2;
+        prev2=prev1;
+      prev1+=temp;
+      } 
+      return prev1;
     }
-    public int findPath(int n, int[] dp){
-         if(n==0) return 1;
-        if(n<0) return 0;
-        if(dp[n] != -1) return dp[n];
-        dp[n]=findPath(n-1,dp)+findPath(n-2,dp);
-        return dp[n];
-    }
+ 
 }
