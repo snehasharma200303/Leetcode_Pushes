0020ab63e443f18b60587b6a976778bd1aed44ba
Sneha Sharma <145634394+snehasharma200303@users.noreply.github.com>
2025-06-28 15:41:32 +0530

    Time: 0 ms (100%), Space: 41.5 MB (18.73%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,17 +1,18 @@
 class Solution {
     public int rob(int[] nums) {
         int len=nums.length;
+        if(len==1) return nums[0];
         int dp1[] =new int[len];
         int dp2[] =new int[len];
         Arrays.fill(dp1,-1);
         Arrays.fill(dp2,-1);
-        int rob_0th=maxMoney(dp1,nums,0,len-2);
-        int rob_1th=maxMoney(dp2,nums,1,len-1);
+        int rob_0th=maxMoney(dp1,nums,0,len-1);
+        int rob_1th=maxMoney(dp2,nums,1,len);
         return Math.max(rob_0th,rob_1th);
     }
     public int maxMoney(int[] dp,int[] nums, int i, int n){
-        if(i==n) return nums[i];
-        if(i>n) return 0;
+        // if(i==n) return nums[i];
+        if(i>=n) return 0;
         if(dp[i]!= -1) return dp[i];
         int step_2=maxMoney(dp,nums,i+2,n)+nums[i];
         int step_1=maxMoney(dp,nums,i+1,n);
