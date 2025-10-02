7a7f08bea23c6eac951d205d13ccdfe9a8cbbdad
Sneha Sharma <145634394+snehasharma200303@users.noreply.github.com>
2025-10-02 09:55:58 +0530

    Time: 8 ms (35.19%), Space: 43.3 MB (48.44%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String reverseWords(String s) {
+        s=s.trim().replaceAll("\\s+"," ");
+        int left=s.length()-1,right=s.length();
+        StringBuilder sb=new StringBuilder();
+        while(left>=0){  
+           char chLeft=s.charAt(left);
+           char chRight=s.charAt(right-1);
+           if(left==0) sb.append(s.substring(left,right));
+           
+            if(chLeft==' '){
+            sb.append(s.substring(left+1,right)+" ");
+            right=left;
+           }
+           left--;
+        }
+        return sb.toString();
+    }
+}
