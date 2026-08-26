32d25d56c9ab3776bc0062dc19d3c0abc0fff5ad
Sneha Sharma <145634394+snehasharma200303@users.noreply.github.com>
2026-08-26 21:30:15 +0530

    Time: 17 ms (16.77%), Space: 52.7 MB (76.09%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,7 +3,7 @@ class Solution {
      HashMap<Integer,Integer>  map=new HashMap<>();
      for(int val:nums){
         map.put(val,map.getOrDefault(val,0)+1);
-        if(map.containsKey(val) && map.get(val)>nums.length/2){
+        if( map.get(val)>nums.length/2){
             return val;
         }
      } 
