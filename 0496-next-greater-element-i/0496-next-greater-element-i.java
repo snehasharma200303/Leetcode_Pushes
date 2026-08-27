f9dbc49e8242903bb359b49628cc373c7b5aa999
Sneha Sharma <145634394+snehasharma200303@users.noreply.github.com>
2026-08-27 16:48:58 +0530

    Time: 3 ms (92.79%), Space: 45.3 MB (36.23%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,19 +1,23 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int n2=nums2.length;
         HashMap<Integer,Integer> map=new HashMap<>();
-        Stack<Integer> st=new Stack<>();
-        for(int i=n2-1;i>=0;i--){
-            while(!st.isEmpty() && st.peek()<nums2[i]) st.pop();
-            if(st.isEmpty()) map.put(nums2[i],-1);
-            else map.put(nums2[i], st.peek());
-            st.push(nums2[i]);
+        for(int i=0;i<nums2.length;i++){
+            map.put(nums2[i],i);
         }
-        int n1=nums1.length;
-        int[] ans=new int[n1];
-        for(int k=0;k<n1;k++){
-            ans[k]=map.get(nums1[k]);
+        int result[]=new int[nums1.length];
+        Arrays.fill(result, -1);
+        for(int i=0;i<result.length;i++){
+            int idx=map.get(nums1[i])+1;
+            while(idx<=nums2.length-1){
+                if(nums1[i]<nums2[idx]) {
+                    result[i]=nums2[idx];
+                    break;
+                    }
+                idx++;
+                
+            }
+
         }
-        return ans;
+        return result;
     }
 }
