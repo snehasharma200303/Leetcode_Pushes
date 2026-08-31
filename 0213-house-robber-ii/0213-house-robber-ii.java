ff0939ca7e89c82cecaf57f18d5054ef3d61c866
Sneha Sharma <145634394+snehasharma200303@users.noreply.github.com>
2026-08-31 16:46:35 +0530

    Time: 65 ms (0.55%), Space: 43 MB (22.92%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,23 +1,18 @@
 class Solution {
     public int rob(int[] nums) {
-        int len=nums.length;
-        if(len==1) return nums[0];
-     int next_0=0;
-     int ans_1=nums[len-2];
-     for(int i=len-3;i>=0;i--){
-        int temp=ans_1;
-        ans_1=nums[i]+next_0;
-        next_0=temp;
-        ans_1=Math.max(ans_1,next_0);
-     }
-    int ans_2=nums[len-1];
-    next_0=0;
-     for(int i=len-2;i>=1;i--){
-        int temp=ans_2;
-        ans_2=nums[i]+next_0;
-        next_0=temp;
-        ans_2=Math.max(ans_2,next_0);
-     }
-     return Math.max(ans_1,ans_2);
+        int n=nums.length;
+        if(n==1) return nums[0];
+        int dp1[]=new int[n];
+        int dp2[]=new int[n];
+        Arrays.fill(dp1,-1);
+        Arrays.fill(dp2,-1);
+       return Math.max(findMax(dp1,nums,0,n-1),findMax(dp2,nums,1,n));
+    }
+    public int findMax(int[] dp,int[] nums,int i, int j){
+        if(i>(j-1)) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int take=nums[i]+findMax(dp,nums,i+2,j);
+        int skip=findMax(dp,nums,i+1,j);
+        return dp[i]= Math.max(take,skip);
     }
 }
