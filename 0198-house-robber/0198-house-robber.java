c526bd267a2289c9b55f55ab08d0879002d8b064
Sneha Sharma <145634394+snehasharma200303@users.noreply.github.com>
2026-08-31 11:53:54 +0530

    Time: 0 ms (100%), Space: 42.9 MB (37.85%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,16 +1,16 @@
 class Solution {
-    public int rob(int[] nums) {
-        int[] dp=new int[nums.length];
-        Arrays.fill(dp,-1);
-       return maxMoney(dp,nums,0);
+    public int rob(int[] nums) { 
+        int dp[]=new int[nums.length];
+        Arrays.fill(dp,-1); 
+        return findMax(dp,nums,0);
+        
     }
-    public int maxMoney(int[] dp,int[] nums, int i){
-        if(i== nums.length-1) return nums[i];
+    public int findMax(int[] dp,int[] nums, int i){
         if(i>=nums.length) return 0;
-        if(dp[i] != -1) return dp[i];
-        int daysPrev=nums[i]+maxMoney(dp,nums,i+2);
-        int daysNext=maxMoney(dp,nums,i+1);
-        dp[i]= Math.max(daysPrev,daysNext);
-        return dp[i];
+        if(dp[i]!=-1) return dp[i];
+      int take=nums[i]+findMax(dp,nums,i+2);
+      int nonTake=findMax(dp,nums,i+1);
+      return dp[i]=Math.max(take,nonTake);
     }
+
 }
