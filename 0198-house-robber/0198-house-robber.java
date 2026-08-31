9b4b6eeff03f20d495fdd8b6c35e78a578a72cbd
Sneha Sharma <145634394+snehasharma200303@users.noreply.github.com>
2026-08-31 12:31:25 +0530

    Time: 0 ms (100%), Space: 42.5 MB (88%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,16 +1,16 @@
 class Solution {
     public int rob(int[] nums) { 
-        int dp[]=new int[nums.length];
-        Arrays.fill(dp,-1); 
-        return findMax(dp,nums,0);
+        int n=nums.length;
+        int dp[]=new int[n];
+        if(n==1) return nums[0];
+        dp[0]=nums[0];
+        dp[1]=Math.max(dp[0],nums[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[n-1];
         
     }
-    public int findMax(int[] dp,int[] nums, int i){
-        if(i>=nums.length) return 0;
-        if(dp[i]!=-1) return dp[i];
-      int take=nums[i]+findMax(dp,nums,i+2);
-      int nonTake=findMax(dp,nums,i+1);
-      return dp[i]=Math.max(take,nonTake);
-    }
+    
 
 }
