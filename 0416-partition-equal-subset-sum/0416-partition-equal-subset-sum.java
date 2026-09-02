6e9e9c16e56ba6276c740ae14328ee9f74333316
Sneha Sharma <145634394+snehasharma200303@users.noreply.github.com>
2026-09-02 16:09:42 +0530

    Time: 24 ms (90.58%), Space: 73 MB (23.55%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,24 +1,19 @@
-public class Solution {
+class Solution {
     public boolean canPartition(int[] nums) {
-        int totalSum = 0;
-        for (int num : nums) totalSum += num;
-        if (totalSum % 2 != 0) return false;
-        int target = totalSum / 2;
-        int n = nums.length;
-        boolean[][] dp = new boolean[n + 1][target + 1];
-
-        for (int i = 0; i <= n; i++) dp[i][0] = true;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= target; j++) {
-                if (nums[i - 1] <= j) {
-                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
-        }
-
-        return dp[n][target];
+        int sum=0;
+       
+        for(int val:nums) sum+=val;
+        if(sum%2!=0) return false;
+         int dp[][]=new int[nums.length][sum/2+1];
+         for(int[] val:dp){
+            Arrays.fill(val,-1);
+         }
+        return (find(dp,sum/2,nums,0)==0)?false:true;
+    }
+    public int find(int[][] dp,int sum,int[] nums,int i){
+        if(sum==0) return 1;
+        if(sum<0 || i>=nums.length) return 0;
+        if(dp[i][sum]!=-1) return dp[i][sum];
+       return dp[i][sum]= (find(dp,sum-nums[i],nums,i+1) ==1 || find(dp,sum,nums,i+1)==1)?1:0;
     }
 }
